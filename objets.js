e7d0f6fa334028747b38174a1a09b646f7c2dfab
Meriton Askaj <meritonaskaj01@gmail.com>
2025-10-24 13:18:01 +0200

    commit 10 : rectification des exo avec le prof

--- objets.js
+++ objets.js
@@ -48,8 +48,13 @@ function createBankAccount () {
     return bankAccount;
 }
 
+const ba = createBankAccount();
+ba.deposit(4);
+ba.withdraw(3);
+ba.getBalance();
+
 //2.2
-function createCar (){
+function createCar (brand, model, year){
     const car = {
         brand: brand,
         model: model,
@@ -59,10 +64,103 @@ function createCar (){
         },
         getAge: function(){
             const currentYear = new Date().getFullYear();
-            return currentYear - this.year;
+            return (currentYear - this.year);
         }
     }
 
     return car;
 }
 
+//3.1
+function createShoopingCart(){
+    const shoppingCart = {
+    items: [],
+    /**
+     * @param article object with properties name and price {name: '', price: 0};
+     */
+    addItem: function(article){
+        this.items.push(article);
+    },
+    removeItem: function(article){
+        const index = this.items.indexOf(article);
+        if (index !== -1){
+            this.items.splice(index, 1);
+        }
+    },
+    getTotal: function() {
+        let total = 0;
+        for (let i = 0; i < this.items.length; i++) {
+        total += this.items[i].price; }
+        return total;
+    }
+}
+    return shoppingCart;
+}
+
+
+//3.2
+function createLibrary (){
+    const library = {
+    books: [],
+    addBook: function(book){
+        this.books.push(book);
+    },
+    findBook: function(title){
+        const bookFounded = this.books.find(book => book.title === title);
+        if (bookFounded){
+            return title + " is in our library";
+        } else {
+            return title + " is not in our library";
+        }
+    },
+    getBooksByAuthor: function (author){
+        return this.book.filter(book => book.author === author);
+    }
+ };
+ return library;
+}
+
+//4.1
+class Student {
+  #name;
+  #notes;
+
+  constructor(name, notes = []) {
+    this.#name = name;
+    this.#notes = notes;
+  }
+
+  get notes() {
+    return this.#notes;
+  }
+
+  set notes(newNote) {
+    this.#notes.push(newNote);
+  }
+
+  getAverage() {
+    let total = 0;
+    for (let note of this.#notes) {
+      total += note;
+    }
+    return total / this.#notes.length;
+  }
+
+  isPassing() {
+    return this.getAverage() >= 10;
+  }
+}
+
+
+//4.2
+class createTimer {
+    #timeLeft;
+
+    constructor (timeLeft = []){
+        this.#timeLeft = timeLeft;
+    }
+
+    start(){
+        let time = this
+    }
+}
